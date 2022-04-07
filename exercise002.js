9ba207f9aa23c22cde70d2204e6e4f5669ef2ae8
Divya <divyabhatt16@gmail.com>
2022-04-07 01:26:57 +0100

    Implemented async and await for fetching data using api end point

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,21 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = 'https://jsonplaceholder.typicode.com/todos/1'
 
-const fetchData = (apiEndPoint) => {
+/*const fetchData = apiEndPoint => {
   fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error))
+}*/
 
-fetchData(jsonTypicode);
+async function fetchData (apiEndPoint) {
+  try {
+    const response = await fetch(apiEndPoint)
+    console.log(await response.json())
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchData(jsonTypicode)
